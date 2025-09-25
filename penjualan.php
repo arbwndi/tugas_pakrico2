2abfdf1f0e0aed2ad6b5f4e35e7bd77897c325a5
arbwndi <akunarimbi6272@gmail.com>
2025-09-25 10:44:47 +0700

    commit 1

---
+++ penjualan.php
@@ -0,0 +1,7 @@
+<?php
+//Setup awal
+echo "--POLGAN MART--<br>";
+echo "Daftar Pembelian<br><br>";
+$nama_barang = ["Floridina", "Kacang dua Kelinci", "Lemonilo", "lays", "Le mineral"];
+$harga_barang = [5000, 1000, 4000, 6000, 3000];
+
